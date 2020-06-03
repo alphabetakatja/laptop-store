ea1fa01fef078bf1ff67777bcd15cc7b3cfce7e7
Katja Stefan <katja.stefan18@gmail.com>
2020-06-03 20:50:38 +0200

    Creating routes for homepage (products) & laptop

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
-    console.log('Someone did access the server!');
+
+    const pathName = url.parse(req.url, true).pathname;
+    console.log(pathName);
+
+    if(pathName === '/products' || pathName === '/') {
+        // Setting up a header
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        // Sending the very response
+        res.end('This is the products page!');
+    }
+    
+    else if (pathName === '/laptop') {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        res.end('This is the laptop page!');
+    }
+    else {
+        res.writeHead(404, { 'Content-type': 'text/html'});
+        res.end('URL was not found on the server!');
+    }
+    
+    
 });
 
 server.listen(1337, '127.0.0.1', () => {
